1a519cd5ddfb47fb80397b69789ac7c1cb871d44
kangmingju <kmg3167@gmail.com>
2021-07-26 17:10:39 +0900

    rere

--- jps/src/main/java/com/jps/controller/CommonExceptionAdvice.java
+++ jps/src/main/java/com/jps/controller/CommonExceptionAdvice.java
@@ -1,4 +1,4 @@
-package com.jps.cotroller;
+package com.jps.controller;
 
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
