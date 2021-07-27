aea851c1a65caf280dee2e6cbe1b6497e7f408d9
Leedohyun <ehgus9443@naver.com>
2021-07-27 16:15:39 +0900

    ..

---
+++ jps/src/main/java/com/jps/controller/UserController.java
@@ -0,0 +1,16 @@
+package com.jps.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping("/user")
+public class UserController {
+
+	private static final Logger logger = LoggerFactory.getLogger(UserController.class);
+	
+	
+}
