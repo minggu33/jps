2be968c3476465776fe5aa94f5622da46b325ed7
kangmingju <kmg3167@gmail.com>
2021-07-27 18:51:38 +0900

    loginform

--- jps/src/main/java/com/jps/controller/UserController.java
+++ jps/src/main/java/com/jps/controller/UserController.java
@@ -1,13 +1,10 @@
 package com.jps.controller;
 
-import java.text.DateFormat;
-import java.util.Date;
-import java.util.Locale;
+
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -15,7 +12,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
  * Handles requests for the application home page.
  */
 @Controller
-@RequestMapping(value = "/user")
+@RequestMapping(value = "/user/*")
 public class UserController {
 	
 	private static final Logger logger = LoggerFactory.getLogger(UserController.class);
@@ -26,14 +23,6 @@ public class UserController {
 	//http://localhost:8088
 	
 	
-	@RequestMapping(value="/login", method= RequestMethod.GET)
-	public String loginGET() throws Exception{
-		logger.info("C : loginGET() 페이지 호출 ");
-		logger.info("C : login view 페이지로 이동 ");
-		
-		
-		return "/loginForm";
-	}
 	
 	@RequestMapping(value = "/join", method = RequestMethod.GET)
 	public void joinGET() {
@@ -41,5 +30,11 @@ public class UserController {
 		logger.info("C : join view 페이지로 이동 ");
 	}
 	
+	
+	@RequestMapping(value = "/login", method = RequestMethod.GET)
+	public String loginGET() {
+		
+		return "/user/loginForm";
+	}
 	
 }
